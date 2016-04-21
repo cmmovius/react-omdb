13c216a564fadd499e05919b98147983376c0ee7
Christine Movius <christine.movius@gmail.com>
2016-04-21 15:19:14 -0400

    Set intial state for input field.

--- app/search.js
+++ app/search.js
@@ -1,10 +1,15 @@
 var React = require("react");
 
 var Search = React.createClass({
+  getInitialState: function() {
+    return {
+      movie: this.props.movie
+    }
+  },
   render: function() {
     return (
       <div>
-        <input type="text" value="Hello!"/>
+        <input type="text" value={this.state.movie}/>
         <input type="submit" value="Inviting"/>
       </div>
     )
