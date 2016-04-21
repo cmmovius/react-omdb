d8c64b05b3191a7cc40bfebff1c91b039262d818
Christine Movius <christine.movius@gmail.com>
2016-04-21 14:47:27 -0400

    Complete initialization and connect to server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + "/app/index.html",
+  filename: "index.html",
+  inject: 'body'
+});
+
 module.exports = {
   // What file to feed into webpack
   entry: [
@@ -17,5 +24,8 @@ module.exports = {
         loader: 'babel-loader' // apply the babel loader
       }
     ]
-  }
+  },
+  plugins: [
+    HtmlWebpackPluginConfig
+  ]
 };
