e4faf2dc47b18c6ed43bbc8603a9d5390ec95731
Christine Movius <christine.movius@gmail.com>
2016-04-21 14:57:02 -0400

    Add Home component.

--- app/index.js
+++ app/index.js
@@ -2,15 +2,31 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 
 // React component definition.
-var Hello = React.createClass({
-  render: function(){
+// var Hello = React.createClass({
+//   render: function(){
+//     return (
+//       <p>Hello world.</p>
+//     );
+//   }
+// })
+//
+// ReactDOM.render(
+//   <Hello />,
+//   document.getElementById( "app" )
+// );
+
+var Home = React.createClass({
+  render: function() {
     return (
-      <p>Hello world.</p>
-    );
+      <div>
+        <h1>OMBD API</h1>
+        <div>Container</div>
+      </div>
+    )
   }
-})
+});
 
 ReactDOM.render(
-  <Hello />,
-  document.getElementById( "app" )
+  <Home />,
+  document.getElementById("app")
 );
